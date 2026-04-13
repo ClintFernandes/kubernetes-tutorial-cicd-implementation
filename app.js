ed5424e0172c9716c61227f95e0c39f90b14e408
clint <clint@bytex.click>
2026-04-13 19:54:59 +0100

    Deployed automatically by CI/CD!

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require('http');
-
 const PORT = 3000;
-const VERSION = 'v1.0.0';
+const VERSION = 'v3.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello from ${VERSION} — My K8s App is running!\n`);
+  res.end(`Hello from ${VERSION} — Deployed automatically by CI/CD! 🎉\n`);
 });
 
 server.listen(PORT, () => {
